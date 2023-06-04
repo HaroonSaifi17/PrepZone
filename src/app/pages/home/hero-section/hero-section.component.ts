1da9bfc57c1d942df5da471c4d5f727d3442b526
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-06-04 14:51:14 +0530

    adding carousel

--- src/app/pages/home/hero-section/hero-section.component.ts
+++ src/app/pages/home/hero-section/hero-section.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
+interface carouselImage {
+  imageSrc: string
+  imageAlt: string
+}
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss']
+  styleUrls: ['./hero-section.component.scss'],
 })
 export class HeroSectionComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
+  selectedIndex: number = 0
+  indicators:boolean=true;
+  controls:boolean=true;
+  images: carouselImage[] = [
+    {
+      imageSrc: './assets/images/img1.jpg',
+      imageAlt: 'Img1.jpg',
+    },
+    {
+      imageSrc: './assets/images/img1.jpg',
+      imageAlt: 'Img1.jpg',
+    },
+    {
+      imageSrc: './assets/images/img1.jpg',
+      imageAlt: 'Img1.jpg',
+    },
+  ]
+  ngOnInit(): void { }
+  selectImage(index:number):void{
+     this.selectedIndex = index;
   }
-
+  onPrevClick():void{
+    if(this.selectedIndex === 0){
+      this.selectedIndex = this.images.length -1;
+    }
+    else{
+      this.selectedIndex--;
+    }
+  }
+  onNextClick():void{
+    if(this.selectedIndex === this.images.length -1){
+      this.selectedIndex = 0;
+    }
+    else{
+      this.selectedIndex++;
+    }
+  }
 }
