d62e015e0d8b5ef928c4b5e6ae97cd26809db1c4
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-06-04 11:38:44 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './pages/home/navbar/navbar.component';
+import { HeroSectionComponent } from './pages/home/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
