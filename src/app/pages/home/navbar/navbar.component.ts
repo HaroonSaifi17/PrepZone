7455db6a97d347474325eb0ba96eca0938dcb226
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-06-17 23:06:47 +0530

    fixing navar for larger displays

--- src/app/pages/home/navbar/navbar.component.ts
+++ src/app/pages/home/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void { }
+  toggleClass(list: HTMLDivElement):void {
+    if (list.style.right == '0') {
+      list.style.right = '100%'
+    } else {
+      list.style.right = '0'
+    }
+    console.log(list.style.right)
+    console.log("hellow");
   }
-
 }
