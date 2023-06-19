724b0d394c2e49fdda26114398a954baba94a0a0
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-06-19 13:29:15 +0530

    adding login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,36 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './pages/home/navbar/navbar.component';
 import { HeroSectionComponent } from './pages/home/hero-section/hero-section.component';
+import { LoginComponent } from './pages/login/login.component';
+import { GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
+import { StudentComponent } from './pages/student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    LoginComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true, //keeps the user signed in
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('94004341890-n5p6bivcp3hgi04q8ssavsst61vpp55a.apps.googleusercontent.com')
+        }
+      ]
+    }
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
