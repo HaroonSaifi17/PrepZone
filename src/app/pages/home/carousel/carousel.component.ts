7fc6de300ba25a6cea52726ee4708d6ea55f2cfc
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-10-23 16:21:05 +0530

    added custom carouel

--- src/app/pages/home/carousel/carousel.component.ts
+++ src/app/pages/home/carousel/carousel.component.ts
@@ -1,54 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 interface carouselImage {
   imageSrc: string
   imageAlt: string
+  imageHeading: string
+  imageText: string
 }
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements OnInit {
-
   constructor() { }
 
+  height: string = '400px'
   selectedIndex: number = 0
-  indicators:boolean=true;
-  controls:boolean=true;
+  indicators: boolean = true
+  controls: boolean = true
+  autoScroll: boolean = true
   images: carouselImage[] = [
     {
-      imageSrc: './assets/images/img1.jpg',
-      imageAlt: 'Img1.jpg',
+      imageSrc: './assets/carousel1.jpg',
+      imageAlt: 'carousel1.jpg',
+      imageHeading: 'This is a Heading1',
+      imageText:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequat suscipit rerum, illum doloremque sed facere nesciunt quas quod! Non,sunt.',
     },
     {
-      imageSrc: './assets/images/img1.jpg',
-      imageAlt: 'Img1.jpg',
+      imageSrc: './assets/carousel1.jpg',
+      imageAlt: 'carousel2.jpg',
+      imageHeading: 'This is a Heading2',
+      imageText:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequat suscipit rerum, illum doloremque sed facere nesciunt quas quod! Non,sunt.',
     },
     {
-      imageSrc: './assets/images/img1.jpg',
-      imageAlt: 'Img1.jpg',
+      imageSrc: './assets/carousel1.jpg',
+      imageAlt: 'carousel2.jpg',
+      imageHeading: 'This is a Heading3',
+      imageText:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequat suscipit rerum, illum doloremque sed facere nesciunt quas quod! Non,sunt.',
     },
   ]
-  ngOnInit(): void { }
-  selectImage(index:number):void{
-     this.selectedIndex = index;
-  }
-  onPrevClick():void{
-    if(this.selectedIndex === 0){
-      this.selectedIndex = this.images.length -1;
-    }
-    else{
-      this.selectedIndex--;
+  ngOnInit(): void {
+    if (this.autoScroll) {
+      setInterval(() => {
+        this.onNextClick()
+      }, 8000)
     }
   }
-  onNextClick():void{
-    if(this.selectedIndex === this.images.length -1){
-      this.selectedIndex = 0;
+  selectImage(index: number): string {
+    if (index == this.selectedIndex) {
+      return 'center'
     }
-    else{
-      this.selectedIndex++;
+    else if(index==this.images.length -1  && this.selectedIndex==0){
+      return 'left'
+    }
+    else if (index==this.selectedIndex-1) {
+       return 'left'
+    }
+    return 'right'
+  }
+  onPrevClick(): void {
+    if (this.selectedIndex === 0) {
+      this.selectedIndex = this.images.length - 1
+    } else {
+      this.selectedIndex--
+    }
+  }
+  onNextClick(): void {
+    if (this.selectedIndex === this.images.length - 1) {
+      this.selectedIndex = 0
+    } else {
+      this.selectedIndex++
     }
   }
 }
