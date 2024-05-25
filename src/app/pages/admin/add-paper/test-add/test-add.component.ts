927c871368108d9253c1a783121c9c30f652a243
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-05-25 15:51:02 +0530

    Fix test generation bug

--- src/app/pages/admin/add-paper/test-add/test-add.component.ts
+++ src/app/pages/admin/add-paper/test-add/test-add.component.ts
@@ -64,7 +64,7 @@ export class TestAddComponent implements OnInit {
       '?exam=' +
       this.exam +
       '&subject=' +
-      this.subject +
+      JSON.stringify(this.subject) +
       '&difficulty=' +
       this.difficulty +
       '&totalQuestions=' +
